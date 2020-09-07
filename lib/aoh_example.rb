faeff32049e90eda19b1fd6966647607385eaeea
Tom ODay <tomoday91@gmail.com>
2020-09-07 20:12:44 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,7 +7,13 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
-  
+  [
+    DON_G, 
+    JOELLE_VD, 
+    PAT_M, 
+    KATE_G, 
+    BRUCE_G
+    ]
 end
 
 def literal_aoh
